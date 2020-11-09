1996483b43d5cc16cda059d59d6725d5a129daa5
archie-ashiton <74156930+archie-ashiton@users.noreply.github.com>
2020-11-09 17:45:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const bot = new Discord.Client();
 
 function game1(){
   bot.user.setActivity('over honey', {type: 'WATCHING'});
+  setTimeout(game2, 10000);
+};
+
+function game2(){
+  bot.user.setActivity('honey tastes good', {type: 'PLAYING'});
+  setTimeout(game1, 10000);
 };
 
 bot.on('ready', () =>{
