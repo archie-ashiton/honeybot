2503d17108323539539d109c35b11f06ce42f27d
archie-ashiton <74156930+archie-ashiton@users.noreply.github.com>
2020-11-09 17:42:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
+function game1(){
+  bot.user.setActivity('over honey', {type: 'WATCHING'});
+};
+
 bot.on('ready', () =>{
     console.log('honey is online!');
+    game1();
   });
 
   bot.login(process.env.token);
