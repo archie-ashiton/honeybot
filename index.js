ce75decf1b745854c0b310ccb0a106e8e2ceb917
archie-ashiton <74156930+archie-ashiton@users.noreply.github.com>
2020-11-10 18:18:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,4 +16,27 @@ bot.on('ready', () =>{
     game1();
   });
 
+  js
+  bot.on('guildMemberAdd', member =>{
+      const channel = member.guild.channels.cache.find(channel => channel.name === '╭・chat');
+      const welcomeEmbed = {
+          color: 0x2F3136,
+          title: `**welcome ${member.displayName}**`,
+          description: 'check out\n・<#775530074030997514>\n・<#775530144693616690>\n・<#775532056456855582>\n・<#775451538059034637>\nto get started <:honey_hj:775473481386950676> ',
+          footer: {
+              text: 'xoxo'
+          },
+          timestamp: new Date()
+      };
+      channel.send(`${member}`);
+      channel.send({ embed: welcomeEmbed });
+      
+  });
+  
+  bot.on('guildMemberRemove', member =>{
+      const channel = member.guild.channels.cache.find(channel => channel.name === '╭・chat');
+      channel.send(`**${member.displayName}** just left the server, bye!<:honey_hj:775473481386950676>`);
+
+  });
+
   bot.login(process.env.token);
